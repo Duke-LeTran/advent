6cbb6f5e86122acb9021a067e20499afa58bbd37
Duke LeTran Win10_Evo500 <daletran@ucdavis.edu>
2021-12-01 20:41:54 -0800

    day 1

---
+++ 1/1_main.py
@@ -0,0 +1,68 @@
+# Author: Duke LeTran
+# Advent of Code 2021
+# Day 1: Sonar
+
+def count_increase(ls:list):
+    """ Part I."""
+    count = 0
+    prev = None
+    for i, ptr in enumerate(ls):
+        if prev is None: # if initializing, save prev
+            prev = ptr
+        if ptr > prev: # if greater, count
+            count += 1
+            print(ptr, '✅', ':: (increased) :: count ::', count, \
+                ':: (ptr)', ptr, ':: (prev)', prev)
+            prev = ptr
+        else: # else do nothing
+            prev = ptr
+            print(ptr, '💔', ':: (decreased) :: count ::', count, \
+                ':: (ptr)', ptr, ':: (prev)', prev)
+    return count
+
+def sliding_window(ls:list):
+    """Part II."""
+    prev = None
+    count = 0
+    for i,j in enumerate(ls[:-2]):
+        window = (ls[i], ls[i+1], ls[i+2])
+        ptr = sum(window)
+        if prev is None: # if initializing, save prev, continue
+            prev = ptr
+            continue
+        if ptr > prev: # if greater count
+            print(ptr, '✅', ':: (increased) :: (count) ::', count, \
+                ':: (ptr, prev)', ptr, prev, ':: (window)', window)
+            count += 1
+            prev = ptr
+        else: # else do nothing
+            if prev == ptr:
+                print(ptr, '🟨', ':: (no change) :: (count) ::', count, \
+                    ':: (ptr, prev)', ptr, prev, ':: (window)', window)
+            else:
+                print(ptr, '💔', ':: (decreased) :: (count) ::', count, \
+                    ':: (ptr, prev)', ptr, prev, ':: (window)', window)
+            prev = ptr
+    return count
+
+def main():
+    # I. Read
+    with open('1_input.txt') as f:
+        # read file, split by newline, convert to int
+        ls = [int(x) for x in f.read().split('\n')]
+    
+    # II. Magic
+    a_count = count_increase(ls)
+    b_count = sliding_window(ls) 
+
+    # III. Answer
+    print('Answers')
+    print('####################')
+    print('# Part I:', a_count)
+    print('# Part II', b_count )
+    
+
+if __name__ == '__main__':
+    main()
+    # Answer 1: 1400
+    # Answer 2: 1429
